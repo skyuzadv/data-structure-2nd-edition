8bb5979bd39442201122b318808b9ac0381497a6
skyuzadv <hxkgfsk3@year.net>
2023-02-28 09:50:11 +0800

    modifly folder

--- ch02/example 2-2 polynomial seqlist.cpp
+++ ch02/example 2-2 polynomial seqlist.cpp
@@ -16,7 +16,7 @@
 
 using namespace std;
 
-#define MAXSIZE 30
+#define MAXSIZE     30
 #define FILE_Import "polynomial seqlist.in"
 #define FILE_Export "polynomial seqlist.ans"
 
@@ -30,31 +30,43 @@ typedef struct ElemType {
 
 // 0.2 线性表
 typedef struct polyno {
-  ElemType *elem; // 指针类型，作基地址
+  ElemType* elem; // 指针类型，作基地址
   int length;     // 表长
 } polyno;
 
 //**********************************************************
 // 函数声明开始
-int isfileNull(const char *);
-template <typename T> bool InitialPolyno(T *ptab);
-template <typename T> void DestoryPolyno(T *ptab);
-template <typename T> void pop_front(T *ptab);
-template <typename T> void pop_back(T *ptab);
-template <typename T> void AddPolyno(T *, T *, T *);
-template <typename T> void SubPolyno(T *, T *, T *);
-template <typename T> void MulPolyno(T *Pa, T *Pb, T *merge);
-template <typename T> bool PolynoShow(T *ptab);
-template <typename T> int GetLength(T *ptab);
-template <typename T1, typename T2> void push_front(T1 *, T2);
-template <typename T1, typename T2> void push_back(T1 *, T2);
-template <typename T1, typename T2> void dataInput(T1 *, T1 *, T2 *);
+int isfileNull(const char*);
+template <typename T>
+bool InitialPolyno(T* ptab);
+template <typename T>
+void DestoryPolyno(T* ptab);
+template <typename T>
+void pop_front(T* ptab);
+template <typename T>
+void pop_back(T* ptab);
+template <typename T>
+void AddPolyno(T*, T*, T*);
+template <typename T>
+void SubPolyno(T*, T*, T*);
+template <typename T>
+void MulPolyno(T* Pa, T* Pb, T* merge);
+template <typename T>
+bool PolynoShow(T* ptab);
+template <typename T>
+int GetLength(T* ptab);
+template <typename T1, typename T2>
+void push_front(T1*, T2);
+template <typename T1, typename T2>
+void push_back(T1*, T2);
+template <typename T1, typename T2>
+void dataInput(T1*, T1*, T2*);
 
 // 函数声明结束
 //**********************************************************
 
 // main()
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   polyno table;
   InitialPolyno(&table);
   int select = 1;
@@ -102,13 +114,12 @@ int main(int argc, char **argv) {
       InitialPolyno<polyno>(&pa);
       InitialPolyno<polyno>(&pb);
 
-      /*
-      printf("请给Pa输入要插入的数据(x, n)（-1,0结束）:>\n");
-while ( scanf("%f %d", &item.coef, &item.expn), item.coef != -1.0f )
-push_back(&pa, item); printf("请给Pb输入要插入的数据(x, n)（-1,0结束）:>\n");
-while ( scanf("%f %d", &item.coef, &item.expn), item.coef != -1.0f )
-push_back(&pb, item);
-      */
+      // printf("请给Pa输入要插入的数据(x, n)（-1,0结束）:>\n");
+      // while (scanf("%f %d", &item.coef, &item.expn), item.coef != -1.0f)
+      //   push_back(&pa, item);
+      // printf("请给Pb输入要插入的数据(x, n)（-1,0结束）:>\n");
+      // while (scanf("%f %d", &item.coef, &item.expn), item.coef != -1.0f)
+      //   push_back(&pb, item);
 
       dataInput<polyno, ElemType>(&pa, &pb, &item);
 
@@ -132,15 +143,15 @@ push_back(&pb, item);
       InitialPolyno<polyno>(&pb);
       InitialPolyno<polyno>(&pc);
       printf("从文件调用数据! \n");
-      FILE *fp;
+      FILE* fp;
       int n1, n2, expn;
       float coef;
       if (isfileNull(FILE_Import)) {
         printf("file is null! please input some data, end with '$' \n");
         fp = fopen(FILE_Import, "w"); // write data
         // clear stdin buffer
-        // fseek(stdin, 0, SEEK_SET);						/*
-        // 清空输入缓冲区 */ rewind(stdin); fflush(stdin);
+        // fseek(stdin, 0, SEEK_SET); /* 清空输入缓冲区 */ rewind(stdin);
+        // fflush(stdin);
         int ch = getchar(); // 读取整数
         int cnt = 1;        // 缓存区存在\n
         while (ch != '$') {
@@ -191,8 +202,8 @@ push_back(&pb, item);
 
 // 判定文本文件是否为空
 // 文件空，返回值为1
-int isfileNull(const char *fileName) {
-  FILE *fp = fopen(fileName, "r");
+int isfileNull(const char* fileName) {
+  FILE* fp = fopen(fileName, "r");
   if (!fp) {
     printf("Can't open file! \n");
     return 1;
@@ -205,13 +216,14 @@ int isfileNull(const char *fileName) {
     return 0;
 }
 // data Input
-template <typename T1, typename T2> void dataInput(T1 *pa, T1 *pb, T2 *item) {
+template <typename T1, typename T2>
+void dataInput(T1* pa, T1* pb, T2* item) {
   printf("++++++++++++++++++++++++++++++++++++\n");
   printf("读取数据:(系数, 指数)格式\n");
   printf("读取选择:\n");
   printf("3>终端输入 4>文件读取 \n");
   printf("++++++++++++++++++++++++++++++++++++\n");
-  FILE *fp;
+  FILE* fp;
   item->coef = 0;
   int select = 1;
   do {
@@ -244,11 +256,12 @@ template <typename T1, typename T2> void dataInput(T1 *pa, T1 *pb, T2 *item) {
       select = 0; // exit case 懒得输入0
     } break;
     case 4: {
-      FILE *fp;
+      FILE* fp;
       int n1, n2;
       if (isfileNull(DATA_Import)) {
-        printf("data file is null, please input some datas in files end with "
-               "'$'! \n");
+        printf(
+            "data file is null, please input some datas in files end with "
+            "'$'! \n");
         fseek(stdin, 0, SEEK_SET); // clean stdin buffer
         fp = fopen(DATA_Import, "w");
         int ch = getchar();
@@ -264,9 +277,11 @@ template <typename T1, typename T2> void dataInput(T1 *pa, T1 *pb, T2 *item) {
         auto expn = item->expn;
         fscanf(fp, "%d", &n1);
         for (int i = 0; i < n1; i++) {
-          //							fscanf(fp, "%f %d", &coef,
-          //&expn); 							pa->elem[pa->length].coef = coef; 							pa->elem[pa->length].expn
-          //= expn; 							pa->length++;
+          //							fscanf(fp, "%f %d",
+          //&coef, &expn);
+          //pa->elem[pa->length].coef = coef;
+          //pa->elem[pa->length].expn = expn;
+          //pa->length++;
           fscanf(fp, "%f %d", &pa->elem[i].coef, &pa->elem[i].expn);
           pa->length++;
         }
@@ -293,7 +308,8 @@ template <typename T1, typename T2> void dataInput(T1 *pa, T1 *pb, T2 *item) {
 }
 
 // 1.0 初始化函数
-template <typename T> bool InitialPolyno(T *ptab) {
+template <typename T>
+bool InitialPolyno(T* ptab) {
   ptab->elem = new ElemType[MAXSIZE]; // 最大容量
   if (!ptab->elem)
     return false;   // 分配失败返回0
@@ -303,7 +319,8 @@ template <typename T> bool InitialPolyno(T *ptab) {
 
 // 1.1 析构函数
 // 销毁所占用内存空间
-template <typename T> void DestoryPolyno(T *ptab) {
+template <typename T>
+void DestoryPolyno(T* ptab) {
   if (!ptab)
     return;
   ptab->length = 0;
@@ -312,7 +329,8 @@ template <typename T> void DestoryPolyno(T *ptab) {
 }
 
 // 2.1 头插
-template <typename T1, typename T2> void push_front(T1 *ptab, T2 item) {
+template <typename T1, typename T2>
+void push_front(T1* ptab, T2 item) {
   if (ptab->length == MAXSIZE)
     return;
   for (int i = ptab->length - 1; i >= 0; i--) {
@@ -325,7 +343,8 @@ template <typename T1, typename T2> void push_front(T1 *ptab, T2 item) {
 }
 
 // 2.2 尾插
-template <typename T1, typename T2> void push_back(T1 *ptab, T2 item) {
+template <typename T1, typename T2>
+void push_back(T1* ptab, T2 item) {
   if (ptab->length == MAXSIZE)
     return;
   if (ptab->elem == NULL)
@@ -334,7 +353,8 @@ template <typename T1, typename T2> void push_back(T1 *ptab, T2 item) {
 }
 
 // 2.3 头删
-template <typename T> void pop_front(T *ptab) {
+template <typename T>
+void pop_front(T* ptab) {
   if (ptab->length == 0)
     return; // if seqlist is empty
   if (ptab->length >= 1) {
@@ -345,7 +365,8 @@ template <typename T> void pop_front(T *ptab) {
 }
 
 // 2.4 尾删
-template <typename T> void pop_back(T *ptab) {
+template <typename T>
+void pop_back(T* ptab) {
   if (ptab->length == 0)
     return;
   if (ptab->length >= 1)
@@ -353,7 +374,8 @@ template <typename T> void pop_back(T *ptab) {
 }
 
 // 3.0 返回表长
-template <typename T> int GetLength(T *ptab) {
+template <typename T>
+int GetLength(T* ptab) {
   if (ptab->elem)
     return ptab->length;
   else
@@ -361,7 +383,8 @@ template <typename T> int GetLength(T *ptab) {
 }
 
 // 4.0 加法
-template <typename T> void AddPolyno(T *Pa, T *Pb, T *Res) {
+template <typename T>
+void AddPolyno(T* Pa, T* Pb, T* Res) {
   T tab;
   tab.elem = new ElemType[Pa->length + Pb->length]; // 创建线性表
   tab.length = 0;                                   // 初始长度为0
@@ -405,7 +428,8 @@ template <typename T> void AddPolyno(T *Pa, T *Pb, T *Res) {
 }
 
 // 4.1 减法
-template <typename T> void SubPolyno(T *Pa, T *Pb, T *Res) {
+template <typename T>
+void SubPolyno(T* Pa, T* Pb, T* Res) {
   T tab;
   /*
   | 	1> 假设多项式Pa与多项式Pb每项次幂均不相同，
@@ -422,7 +446,8 @@ template <typename T> void SubPolyno(T *Pa, T *Pb, T *Res) {
 }
 
 // 4.2 乘法
-template <typename T> void MulPolyno(T *Pa, T *Pb, T *merge) {
+template <typename T>
+void MulPolyno(T* Pa, T* Pb, T* merge) {
   T ptab;
   ptab.elem = new ElemType[Pa->length * Pb->length]; // mul result
   ptab.length = 0;
@@ -438,7 +463,7 @@ template <typename T> void MulPolyno(T *Pa, T *Pb, T *merge) {
   printf("mul result: \n");
   PolynoShow(&ptab);
   // 合并项(升序排序)
-  int *visit = new int[ptab.length]; // 创建标记数组，长度与其等长
+  int* visit = new int[ptab.length]; // 创建标记数组，长度与其等长
   memset(visit, 0, sizeof(int) * ptab.length);
   merge->elem = new ElemType[ptab.length];
   float coef;
@@ -474,7 +499,8 @@ template <typename T> void MulPolyno(T *Pa, T *Pb, T *merge) {
 }
 
 // 8.0 打印
-template <typename T> bool PolynoShow(T *ptab) {
+template <typename T>
+bool PolynoShow(T* ptab) {
   if (!ptab->elem) {
     printf("null \n");
     return false;
